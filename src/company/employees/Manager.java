0a3a0cfe3412c0546334fecb6dd4fc56849517e4
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-03-14 20:57:02 +0200

    Update Manager.java

--- src/company/employees/Manager.java
+++ src/company/employees/Manager.java
@@ -2,5 +2,16 @@
 package company.employees;
 
 public class Manager extends Employee {
+    double bonus;
 
+    public Manager(String Name, int ID, double salary,double bonus) {
+        super(Name, ID, salary);
+        this.bonus = bonus;
+
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Name : " + Name + ", " + "ID : " + ID +", " + "Salary : " + salary + ", " + " Bonus : " + bonus);
+    }
 }
